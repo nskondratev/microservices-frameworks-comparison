566afeec9f7fc677e67cd5e5ccc5120e72c47874
Nikita Kondratev <highflyer16@yandex.ru>
2019-04-07 14:01:35 +0300

    Feature. Implement api service

---
+++ go-micro/api/go.mod
@@ -0,0 +1,20 @@
+module github.com/nskondratev/microservices-frameworks-comparison/go-micro/api
+
+go 1.12
+
+require (
+	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
+	github.com/facebookgo/ensure v0.0.0-20160127193407-b4ab57deab51 // indirect
+	github.com/facebookgo/freeport v0.0.0-20150612182905-d4adf43b75b9 // indirect
+	github.com/facebookgo/grace v0.0.0-20180706040059-75cf19382434
+	github.com/facebookgo/httpdown v0.0.0-20180706035922-5979d39b15c2 // indirect
+	github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 // indirect
+	github.com/facebookgo/stats v0.0.0-20151006221625-1b76add642e4 // indirect
+	github.com/facebookgo/subset v0.0.0-20150612182917-8dac2c3c4870 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.2.8 // indirect
+	github.com/micro/go-micro v1.1.0
+	github.com/micro/go-web v1.0.0
+	github.com/nskondratev/microservices-frameworks-comparison/go-micro/proto v0.0.0-20190407102613-1fe19d652f8d
+	github.com/valyala/fasttemplate v1.0.1 // indirect
+)
---
+++ go-micro/api/service.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"github.com/facebookgo/grace/gracehttp"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-web"
+	. "github.com/nskondratev/microservices-frameworks-comparison/go-micro/proto/hash"
+	"log"
+	"net/http"
+)
+
+type ApiService struct{}
+
+type ErrorJSONResponse struct {
+	Error string `json:"error"`
+}
+
+type SHA256JSONResponse struct {
+	Hash string `json:"hash"`
+}
+
+var (
+	cl HashService
+)
+
+func (as *ApiService) SHA256(c echo.Context) error {
+	log.Println("Received SHA256 API request")
+
+	str := c.Param("string")
+
+	response, err := cl.SHA256(context.TODO(), &SHA256Request{
+		Str: str,
+	})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &ErrorJSONResponse{
+			Error: err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, &SHA256JSONResponse{
+		Hash: response.Hash,
+	})
+}
+
+func main() {
+	service := web.NewService(
+		web.Name("api"),
+	)
+
+	if err := service.Init(); err != nil {
+		log.Fatalf("Error while init API service: %s", err.Error())
+	}
+
+	cl = NewHashService("hash", client.DefaultClient)
+
+	apiService := new(ApiService)
+
+	e := echo.New()
+	e.Use(middleware.CORS())
+	e.Use(middleware.Recover())
+
+	hg := e.Group("/hash")
+	hg.GET("/sha256", apiService.SHA256)
+
+	e.Server.Addr = ":8080"
+	log.Fatal(gracehttp.Serve(e.Server))
+}
